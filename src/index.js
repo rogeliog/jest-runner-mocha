de44b86aa670f024c30475e9ff1ed14361d45ded
Rogelio Guzman <rogelioguzmanh@gmail.com>
2017-08-31 14:49:13 -0700

    It seems to work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const throat = require('throat');
+const pify = require('pify');
+const workerFarm = require('worker-farm');
 const execa = require('execa');
 const path = require('path');
+
+const TEST_WORKER_PATH = path.join(__dirname, 'runMocha.js');
 
 class CancelRun extends Error {
   constructor(message) {
@@ -16,32 +20,70 @@ class MochaTestRunner {
 
   // eslint-disable-next-line
   async runTests(tests, watcher, onStart, onResult, onFailure /* , options */) {
+    const farm = workerFarm(
+      {
+        autoStart: true,
+        maxConcurrentCallsPerWorker: 1,
+        maxConcurrentWorkers: this._globalConfig.maxWorkers,
+        maxRetries: 2, // Allow for a couple of transient errors.
+      },
+      TEST_WORKER_PATH,
+    );
     const mutex = throat(this._globalConfig.maxWorkers);
-    return Promise.all(
+    const worker = pify(farm);
+    const runTestInWorker = test =>
+      mutex(async () => {
+        if (watcher.isInterrupted()) {
+          throw new CancelRun();
+        }
+        await onStart(test);
+        return worker({
+          config: test.context.config,
+          globalConfig: this._globalConfig,
+          path: test.path,
+          rawModuleMap: watcher.isWatchMode()
+            ? test.context.moduleMap.getRawModuleMap()
+            : null,
+        });
+      });
+
+    const onError = async (err, test) => {
+      await onFailure(test, err);
+      if (err.type === 'ProcessTerminatedError') {
+        console.error(
+          'A worker process has quit unexpectedly! ' +
+            'Most likely this is an initialization error.',
+        );
+        process.exit(1);
+      }
+    };
+
+    const onInterrupt = new Promise((_, reject) => {
+      watcher.on('change', state => {
+        if (state.interrupted) {
+          reject(new CancelRun());
+        }
+      });
+    });
+
+    const runAllTests = Promise.all(
       tests.map(test =>
-        mutex(async () => {
-          if (watcher.isInterrupted()) {
-            throw new CancelRun();
-          }
-
-          await onStart(test);
-
-          return this._runTest(test)
-            .then(result => {
-              // console.log(result);
-              onResult(test, result);
-            })
-            .catch(e => onFailure(test, e));
-        }),
+        runTestInWorker(test)
+          .then(testResult => onResult(test, testResult))
+          .catch(error => onError(error, test)),
       ),
     );
+
+    const cleanup = () => workerFarm.end(farm);
+
+    return Promise.race([runAllTests, onInterrupt]).then(cleanup, cleanup);
   }
 
   // eslint-disable-next-line
   async _runTest(test) {
-    return execa('node', [path.join(__dirname, 'runMocha.js'), test.path], {
-      env: process.env,
-    }).then(({ stdout }) => JSON.parse(stdout));
+    // return execa('node', , test.path], {
+    //   env: process.env,
+    // }).then(({ stdout }) => JSON.parse(stdout));
     // .then(console.log); // JSON.parse(stdout));
     // })
     //   .then(a => console.log(a))
--- src/runMocha.js
+++ src/runMocha.js
@@ -1,8 +1,9 @@
 const Mocha = require('mocha');
 const omit = require('lodash/omit');
+const path = require('path');
 const toTestResult = require('./utils/toTestResult');
 
-const testPath = process.argv[2];
+// const testPath = process.argv[2];
 
 function clean(test) {
   return {
@@ -14,46 +15,54 @@ function clean(test) {
   };
 }
 
-class Reporter extends Mocha.reporters.Base {
-  constructor(runner) {
-    super(runner);
-    const tests = [];
-    const pending = [];
-    const failures = [];
-    const passes = [];
+const runMocha = (options, workerCallback) => {
+  class Reporter extends Mocha.reporters.Base {
+    constructor(runner) {
+      super(runner);
+      const tests = [];
+      const pending = [];
+      const failures = [];
+      const passes = [];
 
-    runner.on('test end', test => tests.push(test));
-    runner.on('pass', test => passes.push(test));
-    runner.on('fail', test => failures.push(test));
-    runner.on('pending', test => pending.push(test));
-    runner.on('end', () => {
-      const results = {
-        stats: this.stats,
-        tests: tests.map(clean),
-        pending: pending.map(clean),
-        failures: failures.map(clean),
-        passes: passes.map(clean),
-        jestTestPath: testPath,
-      };
+      runner.on('test end', test => tests.push(test));
+      runner.on('pass', test => passes.push(test));
+      runner.on('fail', test => failures.push(test));
+      runner.on('pending', test => pending.push(test));
+      runner.on('end', () => {
+        const results = {
+          stats: this.stats,
+          tests: tests.map(clean),
+          pending: pending.map(clean),
+          failures: failures.map(clean),
+          passes: passes.map(clean),
+          jestTestPath: options.path,
+        };
 
-      try {
-        // console.log(results.failures[0].err instanceof Error);
-        process.stdout.write(JSON.stringify(toTestResult(results), null, 2));
-      } catch (e) {
-        console.log(e);
-      }
-    });
+        try {
+          workerCallback(null, toTestResult(results));
+        } catch (e) {
+          workerCallback(e);
+        }
+      });
+    }
+
+    // eslint-disable-next-line
+    // epilogue() {}
   }
 
-  // eslint-disable-next-line
-  // epilogue() {}
-}
-
-const mocha = new Mocha({ reporter: Reporter });
-mocha.addFile(testPath);
-
-mocha.run(() => {
-  process.on('exit', () => {
-    process.exit();
+  const mocha = new Mocha({
+    // ui: 'tdd',
+    reporter: Reporter,
   });
-});
+
+  require('/Users/rogelioguzman/dev/babel/scripts/babel-register');
+  mocha.addFile(options.path);
+
+  mocha.ui('tdd').run(() => {
+    process.on('exit', () => {
+      process.exit();
+    });
+  });
+};
+
+module.exports = runMocha;
